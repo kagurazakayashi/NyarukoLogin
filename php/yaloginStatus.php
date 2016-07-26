412ae37a655d6a2c6cdd547d58c3f2cdab6428d3
神楽坂雅詩 <cyscxchope@gmail.com>
2016-07-26 15:14:14 +0800

    制作登录状态查询类

--- php/yaloginStatus.php
+++ php/yaloginStatus.php
@@ -2,15 +2,49 @@
 /**
 * 登录状态检查类
 */
+require 'yaloginUserInfo.php';
+require 'YaloginSQLSetting.php';
 class YaloginStatus
 {
-	function loaduserstatus()
+	private $user;
+	private $sqlset;
+
+	function init()
 	{
-		require 'yaloginSQLSetting.php';
-		$sqlSetting = new YaloginSQLSetting();
-		
+		$this->user = new YaloginUserInfo();
+		$this->sqlset = new YaloginSQLSetting();
+	}
+
+	//登录状态
+	function loginuser() {
+		//检查登录 SESSION
+		$cookiejson = isset($_SESSION["logininfo"]) ? $_SESSION["logininfo"] : null;
+		if ($cookiejson == null) {
+			//检查登录 COOKIE
+			$cookiename = $this->cookiename();
+			$cookiejson = isset($_COOKIE[$cookiename]) ? $_COOKIE[$cookiename] : null;
+			if ($cookiejson == null) {
+				return null
+			} else {
+				$_SESSION["logininfo"] = $cookiejson;
+			}
+		}
+		//取登录信息
+		$cookiejsonarr = json_decode($cookiejson);
+		if (isset($_SESSION["sessiontoken"]) && 
+		isset($_SESSION["sessionname"]) && 
+		isset($_SESSION["sessionid"]) && 
+		isset($_SESSION["username"]) && 
+		isset($_SESSION["userhash"]) && 
+		isset($_SESSION["lifetime"]))
+		return $cookiejsonarr;
+	}
+
+	function cookiename($key = "") {
+		if (strlen($key) > 0) {
+			$key = "_".$key;
+		}
+		return "yalogin_".$this->sqlset->db_app.$key;
 	}
 }
-$loginStatus = new YaloginStatus();
-$loginStatus->loaduserstatus();
 ?>
