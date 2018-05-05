5fb95109ac3c01cc4d5972a19748c27327d0b15e
神楽坂雅詩 <cyscxchope@gmail.com>
2018-05-05 14:35:52 +0800

    完成数据库连接测试

---
+++ tests/sqlconnect.php
@@ -0,0 +1,13 @@
+<?
+header('Content-Type: text/plain; charset=utf-8');
+echo "【数据库连接测试】\n";
+echo "引入文件 nyaconnect.class.php ... ";
+require_once "../nyaconnect.class.php";
+echo "完成。\n";
+echo "初始化 nyadbconnect ... ";
+$nyadbconnect = new nyadbconnect();
+echo "完成。\n";
+echo "执行 nyaconnect_sqltest() ... \n";
+echo "数据库版本: ".$nyadbconnect->nyaconnect_sqltest();
+echo "\n完成。\n";
+?>
