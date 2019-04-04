757e2e16811041332f6789bac64ea42f10663859
神楽坂雅詩 <cyscxchope@gmail.com>
2019-04-04 15:14:17 +0800

    检查和准备用户注册需要用的各种信息

--- src/nyasignup.class.php
+++ src/nyasignup.class.php
@@ -1,6 +1,7 @@
 <?php
 require_once "nyacaptcha.class.php";
 require_once "nyaverification.class.php";
+require_once "nyauser.class.php";
 class nyasignup {
     var $nyadbconnect;
     function __construct() {
@@ -35,27 +36,60 @@ class nyasignup {
         $nyacaptcha = new nyacaptcha();
         // if (!$nyacaptcha->verifycaptcha($totptoken,$totpsecret,$jsonarr["captcha"])) die();
         //检查输入的是邮箱还是手机号
+        $nyauser = new nyauser();
         $user = $jsonarr["user"];
-        $logintype = 0; //1:手机号 2:邮箱
-        if ($nlcore->safe->isPhoneNumCN($user)) {
-            $logintype = 1;
-        } else if ($nlcore->safe->isEmail($user)) {
-            $logintype = 2;
-        } else {
-            die($nlcore->msg->m(1,2020206));
+        $logintype = $nyauser->logintype($user); //0:邮箱 1:手机号
+        //检查是否允许使用这种方式注册
+        if (!$nlcore->cfg->app->logintype[$logintype]) $nlcore->msg->http403(2040103);
+        //检查输入格式是否正确
+        $newuserconf = $nlcore->cfg->app->newuser;
+        if ($logintype == 0 && !$nlcore->safe->isNumberOrEnglishChar(5,$newuserconf["emaillen"],$user)) {
+            $nlcore->msg->http403(2020207);
+        } else if ($logintype == 1 && !$nlcore->safe->isNumberOrEnglishChar(11,11,$user)) {
+            $nlcore->msg->http403(2020205);
         }
-        
-
-
-
-
+        //检查密码强度是否符合规则
+        $password = $jsonarr["password"];
+        $nlcore->safe->strongpassword($password);
+        //检查昵称
+        $nickname = $jsonarr["nickname"];
+        $nicknamelen = mb_strlen($nickname,"utf-8");
+        //如果没有昵称使用电子邮件前缀，如果不是电子邮件使用默认昵称加随机数
+        if ($nicknamelen < 1) {
+            if ($logintype == 0) {
+                $nickname = explode("@", $user)[0];
+            } else {
+                $nickname = $newuserconf["nickname"].rand(100, 999);
+            }
+        } else if ($nicknamelen > $newuserconf["nicknamelen"]) { //检查昵称是否太长
+            $nlcore->msg->http403(2040105);
+        } else {
+            $nlcore->safe->wordfilter($nickname); //检查敏感词
+        }
+        //生成账户代码，遇到重复的重试10次
+        $nameid = 0;
+        for ($i=0; $i < 10; $i++) { 
+            $nameid = rand(0, 9999);
+            //检查昵称和状态代码是否重复
+            if (!$nyauser->useralreadyexists(null,$nickname,$nameid)) break;
+            if ($i == 10) $nlcore->msg->http403(2040200);
+        }
+        //检查邮箱或者手机号是否已经重复
+        $isalreadyexists = $nyauser->isalreadyexists($logintype,$user);
+        if ($isalreadyexists == 1) $nlcore->msg->http403(2040102);
+        //分配预设的用户组
+        $user_group = $newuserconf["group"];
+        //生成密码到期时间
+        $password_expiration = time() + $newuserconf["pwdexpiration"];
+        $password_expiration = $nyauser->getdatetime(null,$password_expiration)[1];
+        //注册 users,password_protection 表
 
 
         if ($nlcore->safe->isPhoneNumCN($user)) {
-            //注册 users 表
+            
             //TODO: 短信注册流程
         } else if ($nlcore->safe->isEmail($user)) {
-            //注册 users 表
+
             $nyaverification = new nyaverification();
             $mailinfo = $nyaverification->sendmail(); //[$mailhtml,$vcode]
         } else {
