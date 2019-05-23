77f3e4e8519ab96a378572dce438798047ea46f2
神楽坂雅詩 <cyscxchope@gmail.com>
2019-05-23 13:10:17 +0800

    对上传的图片进行尺寸压缩

--- src/uploadfile.class.php
+++ src/uploadfile.class.php
@@ -16,21 +16,20 @@ class uploadfile {
         foreach ($files as $nowfile) {
             $mediatype = $this->chkfile($nowfile,$uploadconf); //检查文件
             $code = 1000000;
+            $tmpfile = $nowfile["tmp_name"];
             if (is_numeric($mediatype)) $code = $mediatype; //返回错误
             $newfilename = $nlcore->safe->randstr(64); //创建文件名
             $twotmpfile = $uploadconf["tmpdir"];
             $savefile = $savedir.$newfilename; //最终存储文件完整路径
             $savetmpfile = $stmpdir.$newfilename; //最终存储临时文件完整路径
             //是视频还是图片？
-            echo json_encode($mediatype);
+            // echo json_encode($mediatype); //["image/jpeg","jpg","image"]
             if ($mediatype == "image") {
                 //如果是图片文件则直接进行二压
             } else if ($mediatype == "video") {
                 //如果是视频文件则移动到临时目录进行二压，生成临时操作指令
             }
-
-
-
+            $this->resizeto($tmpfile,$savetmpfile,$mediatype[1],[320,240]);
 
             $nowvideoarr = [
                 "code" => $code,
@@ -40,6 +39,83 @@ class uploadfile {
         //return array_merge($nowreturnarr,$nlcore->msg->m(0,2050100));
     }
 
+    /**
+     * @description: 缩小图片（如果已经小于设定值则输出原尺寸）
+     * @param Float imageWidth 原始图片宽度
+     * @param Float imageHeight 原始图片高度
+     * @param Float maxWidth 目标尺寸宽度
+     * @param Float maxHeight 目标尺寸高度
+     * @return Array<Float> 新的宽高
+     */
+    function getresize($imageWidth,$imageHeight,$maxWidth,$maxHeight) {
+        $newWidth = 0;
+        $newHeight = 0;
+        $imageScale = $imageWidth / $imageHeight;
+        $maxScale = $maxWidth / $maxHeight;
+        // echo json_encode([$maxScale,$imageScale]);
+        // echo json_encode([$maxWidth,$maxHeight]);
+        // echo json_encode([$imageWidth,$imageHeight]);
+        if ($maxScale < $imageScale) {
+            // echo "111|";
+            $newWidth = $maxWidth;
+            $newHeight = $maxWidth / $imageScale;
+        } else if ($maxScale > $imageScale) {
+            // echo "222|";
+            $newHeight = $maxHeight;
+            $newWidth = $maxHeight * $imageScale;
+        }
+        // echo json_encode([$newWidth,$newHeight]);
+        echo $newWidth.'x'.$newHeight;
+        return [$newWidth,$newHeight];
+    }
+
+    function resizeto($imagefile,$tofile,$type,$size) {
+        $srcImg = null;
+        $isGIF = false;
+        switch ($type) {
+            case 'jpg':
+                $srcImg = imagecreatefromjpeg($imagefile);
+                break;
+            case 'png':
+                $srcImg = imagecreatefrompng($imagefile);
+                break;
+            case 'gif':
+                $srcImg = imagecreatefromgif($imagefile);
+                $isGIF = true;
+                break;
+            case 'webp':
+                $srcImg = imagecreatefromwebp($imagefile);
+                break;
+            case 'bmp':
+                $srcImg = imagecreatefrombmp($imagefile);
+                break;
+            default:
+                break;
+        }
+        if (!$srcImg) return false;
+        $srcWidth = imagesx($srcImg);
+        $srcHeight = imagesy($srcImg);
+        $newsize = $this->getresize($srcWidth,$srcHeight,$size[0],$size[1]);
+        $newImg = imagecreatetruecolor($newsize[0], $newsize[1]);
+        echo count($srcImg);
+        $alpha = imagecolorallocatealpha($newImg, 0, 0, 0, 127);
+        imagefill($newImg, 0, 0, $alpha);
+        imagecopyresampled($newImg, $srcImg, 0, 0, 0, 0, $newsize[0], $newsize[1], $srcWidth, $srcHeight);
+        imagesavealpha($newImg, true);
+        // imagepng($newImg, $tofile);
+        // imagewebp($newImg, $tofile.'.webp');
+        imagegif($newImg, $tofile.'.gif');
+        imagedestroy($newImg);
+        return true;
+    }
+
+    /**
+     * @description: 检查文件是否符合要求
+     * @param String nowfile 当前文件完整路径
+     * @param Array uploadconf 上传配置数组
+     * @param Array enable 允许上传的媒体类别，默认["image","video"]都可以
+     * @return Array [MIME类型,扩展名,image还是video]
+     */
     function chkfile($nowfile,$uploadconf,$enable=["image","video"]) {
         $nowreturnarr = [
             "name" => $nowfile["name"]
