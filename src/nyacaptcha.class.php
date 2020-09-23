57d01eaf4f80dbc973e51743e54672325d62c126
神楽坂雅詩 <cyscxchope@gmail.com>
2020-09-23 18:32:05 +0800

    图形验证码支持从Redis存入和读出

--- src/nyacaptcha.class.php
+++ src/nyacaptcha.class.php
@@ -12,17 +12,17 @@ class nyacaptcha {
     function __construct() {
     }
     /**
-     * @description: 创建验证码
-     * @param Bool extnow 是否立即将信息返回给客户端
-     * @param Bool showcaptcha 是否直接返回验证码明码，而不是图片
-     * @param Bool showimage 是否直接输出验证码图片
-     * @return Array<String> 验证码相关信息：
-     * code 状态码
-     * time 验证码生成时间
-     * img 验证码文件名（不包括扩展名和路径）
-     * captcha 验证码内容
-     * file 验证码图片本地存储路径(extnow 时不输出)
-     * url 验证码图片网址
+     * @description: 建立驗證碼
+     * @param Bool extnow 是否立即將資訊返回給客戶端
+     * @param Bool showcaptcha 是否直接返回驗證碼明碼，而不是圖片
+     * @param Bool showimage 是否直接輸出驗證碼圖片
+     * @return Array<String> 驗證碼相關資訊：
+     * code 狀態碼
+     * time 驗證碼生成時間
+     * img 驗證碼檔名（不包括副檔名和路徑）
+     * captcha 驗證碼內容
+     * file 驗證碼圖片本地儲存路徑(extnow 時不輸出)
+     * url 驗證碼圖片網址
      */
     function getcaptcha($extnow = true, $showcaptcha = false, $showimage = false) {
         global $nlcore;
@@ -47,16 +47,32 @@ class nyacaptcha {
             $builder->save($imgpath);
         }
         $vc1code = $builder->getPhrase();
-        //写入数据库
-        $updateDic = [
-            "vc1code" => $vc1code,
-            "vc2time" => $time
-        ];
-        $tableStr = $nlcore->cfg->db->tables["encryption"];
-        $whereDic = [
-            "apptoken" => $appToken
-        ];
-        $dbreturn = $nlcore->db->update($updateDic, $tableStr, $whereDic);
+        if ($nlcore->db->initRedis()) {
+            // 嘗試儲存到 Redis
+            $timeout = $nlcore->cfg->verify->timeout["captcha"];
+            $redis = $nlcore->db->redis;
+            $key = $this->redisKeyName();
+            // 檢查是否有驗證碼資料
+            if ($redis->exists($key)) {
+                $redis->del($key); // 有則刪除
+            }
+            // 儲存驗證碼
+            $redis->setex($key, $timeout, $vc1code);
+        } else {
+            // 嘗試儲存到 MySQL
+            $updateDic = [
+                "vc1code" => $vc1code,
+                "vc1time" => $time
+            ];
+            $tableStr = $nlcore->cfg->db->tables["encryption"];
+            $whereDic = [
+                "apptoken" => $appToken
+            ];
+            $result = $nlcore->db->update($updateDic, $tableStr, $whereDic);
+            if ($result[0] >= 2000000) {
+                $nlcore->msg->stopmsg(2020601);
+            }
+        }
         $retuenarr = [
             "code" => 1000000,
             "img" => $imgurl
@@ -65,7 +81,7 @@ class nyacaptcha {
             $retuenarr["captcha"] = $vc1code;
         }
         if ($debug) {
-            $retuenarr["debug"] = $debug;
+            $retuenarr["debug"] = $vc1code;
         }
         if ($extnow) {
             echo $nlcore->sess->encryptargv($retuenarr);
@@ -74,11 +90,18 @@ class nyacaptcha {
         }
         return null;
     }
-
     /**
-     * @description: 验证码验证失败后用此函数重新创建一个
-     * @param String code 错误代码
-     * @return Array<String> 验证码相关信息（其中code、msg会不同）
+     * @description: 建立 Redis 鍵名
+     * @return String 鍵名
+     */
+    function redisKeyName(): string {
+        global $nlcore;
+        return $nlcore->cfg->db->redis_tables["vcode1"] . $nlcore->sess->appToken;
+    }
+    /**
+     * @description: 驗證碼驗證失敗後用此函式重新建立一個
+     * @param String code 錯誤程式碼
+     * @return Array<String> 驗證碼相關資訊（其中code、msg會不同）
      */
     function verifyfailgetnew($code) {
         global $nlcore;
@@ -90,42 +113,63 @@ class nyacaptcha {
     }
 
     /**
-     * @description: 验证图形验证码是否正确
-     * @param String captchacode 验证码
-     * @param String totpsecret totp加密码
+     * @description: 驗證圖形驗證碼是否正確
+     * @param String captchacode 驗證碼
+     * @param String totpsecret totp加密碼
      * @return Bool 是否可以通行
      */
     function verifycaptcha($appToken, $captchacode) {
+        if (strlen($captchacode) < 4) return false;
         global $nlcore;
-        $columnArr = ["id", "vc1code", "vc1time"];
-        $tableStr = $nlcore->cfg->db->tables["encryption"];
-        $whereDic = [
-            "apptoken" => $appToken
-        ];
-        $dbreturn = $nlcore->db->select($columnArr, $tableStr, $whereDic);
-        if ($dbreturn[0] != 1010000) {
-            die($nlcore->msg->m(2020501));
+        if ($nlcore->db->initRedis()) {
+            // 嘗試從 Redis 載入
+            $redis = $nlcore->db->redis;
+            $key = $this->redisKeyName();
+            // 檢查是否有驗證碼資料
+            if ($redis->exists($key)) {
+                $saveCode = $redis->get($key);
+                // 檢查驗證碼是否匹配
+                if (strtolower($captchacode) != strtolower($saveCode)) {
+                    $this->verifyfailgetnew(2020505); // 不匹配
+                    return false;
+                }
+                // 刪除已經驗證透過的資訊
+                $redis->del($key);
+            } else {
+                $nlcore->msg->stopmsg(2020505);
+            }
+        } else {
+            // 嘗試從 MySQL 載入
+            $columnArr = ["id", "vc1code", "vc1time"];
+            $tableStr = $nlcore->cfg->db->tables["encryption"];
+            $whereDic = [
+                "apptoken" => $appToken
+            ];
+            $dbreturn = $nlcore->db->select($columnArr, $tableStr, $whereDic);
+            if ($dbreturn[0] != 1010000) {
+                die($nlcore->msg->m(2020501));
+            }
+            $cinfo = $dbreturn[2][0];
+            $vc1time = strtotime($cinfo["vc1time"]);
+            $endtime = $vc1time + $nlcore->cfg->verify->timeout["captcha"];
+            if (time() > $endtime) {
+                $this->verifyfailgetnew(2020502);
+                return false;
+            }
+            if (strtolower($captchacode) != strtolower($cinfo["vc1code"])) {
+                $this->verifyfailgetnew(2020503);
+                return false;
+            }
+            // 刪除已經驗證透過的資訊
+            $updateDic = [
+                "vc1code" => null,
+                "vc1time" => null
+            ];
+            $whereDic = [
+                "id" => $cinfo["id"]
+            ];
+            $dbreturn = $nlcore->db->update($updateDic, $tableStr, $whereDic);
         }
-        $cinfo = $dbreturn[2][0];
-        $vc1time = strtotime($cinfo["vc1time"]);
-        $endtime = $vc1time + $nlcore->cfg->verify->timeout["captcha"];
-        if (time() > $endtime) {
-            $this->verifyfailgetnew(2020502);
-            return false;
-        }
-        if (strtolower($captchacode) != strtolower($cinfo["vc1code"])) {
-            $this->verifyfailgetnew(2020503);
-            return false;
-        }
-        //删除已经验证通过的信息
-        $updateDic = [
-            "vc1code" => null,
-            "vc1time" => null
-        ];
-        $whereDic = [
-            "id" => $cinfo["id"]
-        ];
-        $dbreturn = $nlcore->db->update($updateDic, $tableStr, $whereDic);
         return true;
     }
 }
